22aec5c87e19e055f1366b64680078f1b701a046
Kra Larivain <olarivain@gmail.com>
2013-11-10 23:10:56 -0800

    - some more constraints on the window resizing. Close enough for now.

--- RestEasy/Home/MainWindowController.m
+++ RestEasy/Home/MainWindowController.m
@@ -12,7 +12,7 @@
 
 #import "Request.h"
 
-@interface MainWindowController ()<NSSplitViewDelegate>
+@interface MainWindowController ()<NSWindowDelegate, NSSplitViewDelegate>
 // main views
 @property (weak) IBOutlet NSSplitView *splitView;
 @property (weak) IBOutlet NSScrollView *projectView;
@@ -85,15 +85,39 @@
     return [Request requestWithURL: url method: method headers: headers body: self.responseBodyTextView.string];
 }
 
+#pragma mark - NSWindowDelegate
+-(CGSize)windowWillResize:(NSWindow *)sender toSize:(NSSize)frameSize
+{
+    CGFloat minWidth = self.projectView.rez_widthConstraint.constant + self.restView.rez_widthConstraint.constant;
+    minWidth += self.splitView.dividerThickness;
+    
+    if(frameSize.width <= minWidth) {
+        return CGSizeMake(minWidth, frameSize.height);
+    }
+    
+    return frameSize;
+}
+
 #pragma mark - NSSplitViewdDelegate
 - (BOOL) splitView:(NSSplitView *)splitView shouldAdjustSizeOfSubview:(NSView *)view
 {
+    // prevent the left pane from resizing when the divider is moved around
 	return view == [splitView.subviews rez_objectAtIndex: 1];
 }
 
--(CGFloat)splitView:(NSSplitView *)splitView constrainSplitPosition:(CGFloat)proposedPosition ofSubviewAt:(NSInteger)dividerIndex {
+-(CGFloat)splitView:(NSSplitView *)splitView constrainSplitPosition:(CGFloat)proposedPosition ofSubviewAt:(NSInteger)dividerIndex
+{
     // make sure the split view honors the rest view's min width
     CGFloat maxPosition = self.window.frame.size.width - self.restView.rez_widthConstraint.constant;
+    maxPosition -= self.splitView.dividerThickness;
+    
+    CGFloat minPosition = self.projectView.rez_widthConstraint.constant;
+    minPosition += self.splitView.dividerThickness;
+    
+    if(proposedPosition <= minPosition) {
+        return minPosition;
+    }
+    
     if(maxPosition <= proposedPosition) {
         return maxPosition;
     }
